69b4e7796a2ed098a9378ed95b0e277c5ad6980e
aaronmunsters <aaronmunstersbxl@gmail.com>
2021-12-07 18:12:30 +0100

    Removed ugly parsing, moved to typesafe case classes and sealed traits.

--- src/main/scala/AST/ReplicatedOperation.scala
+++ src/main/scala/AST/ReplicatedOperation.scala
@@ -1,8 +1,7 @@
 package AST
 
 import AST.Edit._
-import AST.Node.{SchemeExpression, SchemeIdentifier, SchemeNode, SchemeNumber}
-import AST.ReplicatedStringOp.encodeTree
+import AST.Node.{SchemeExpression, SchemeIdentifier, SchemeNumber, SchemeString}
 import boopickle.Default._
 
 import java.nio.ByteBuffer
@@ -12,32 +11,62 @@ trait ReplicatedOperation[Identity, EditIdentity] {
   val edit: AstEdit[Identity]
 }
 
-protected case class RepOp(ei: (Int, Int), e: Array[Int], tree: Option[Array[Int]])
+sealed trait SerializableAstEdit
+
+sealed trait SerializableAst
+
+private sealed case class RepIntOp(editIdentity: (Int, Int), edit: SerializableAstEdit)
+
+object SerializableAstEdit {
+  case class Add(tree: SerializableAst, parent: Option[Int], index: Int) extends SerializableAstEdit
+  case class Delete(target: Int)                                         extends SerializableAstEdit
+  case class Move(child: Int, newParent: Int, index: Int)                extends SerializableAstEdit
+  case class UpdateString(target: Int, value: Array[Char])               extends SerializableAstEdit
+  case class UpdateNumber(target: Int, value: Long)                      extends SerializableAstEdit
+
+  case class AstExpression(id: Int, parent: Option[Int], children: Array[Int]) extends SerializableAst
+  case class AstIdentifier(id: Int, parent: Option[Int], value: Array[Char])   extends SerializableAst
+  case class AstNumber(id: Int, parent: Option[Int], value: Long)              extends SerializableAst
+  case class AstString(id: Int, parent: Option[Int], value: Array[Char])       extends SerializableAst
+
+  def astEditToSerializableEdit(astEdit: AstEdit[Int]): SerializableAstEdit = astEdit match {
+    case delete: AstEdit.Delete[Int] => Delete(delete.target)
+    case move: AstEdit.Move[Int] => Move(move.child, move.newParent, move.index)
+    case value: AstEdit.UpdateValue[Int] => value.value match {
+      case s: String => UpdateString(value.target, s.toCharArray)
+      case n: Long => UpdateNumber(value.target, n)
+    }
+    case add: AstEdit.Add[Int] =>
+      val serTree = add.tree match {
+        case expression: SchemeExpression[Int] => AstExpression(expression.id, expression.parent, expression.children.toArray)
+        case identifier: SchemeIdentifier[Int] => AstIdentifier(identifier.id, identifier.parent, identifier.value.toCharArray)
+        case number: SchemeNumber[Int] => AstNumber(number.id, number.parent, number.value)
+        case string: SchemeString[Int] => AstString(string.id, string.parent, string.value.toCharArray)
+      }
+      Add(serTree, add.parent, add.index)
+  }
+
+  def serializedEditToEdit(serEdit: SerializableAstEdit): AstEdit[Int] = serEdit match {
+    case Delete(target) => Edit.Delete(target)
+    case Move(child, newParent, index) => Edit.Move(child, newParent, index)
+    case UpdateString(target, value) => Edit.UpdateValue(target, value.mkString(""))
+    case UpdateNumber(target, value) => Edit.UpdateValue(target, value)
+    case Add(tree, parent, index) =>
+      val hydratedTree = tree match {
+        case AstExpression(id, parent, children) => SchemeExpression(id, parent, children)
+        case AstIdentifier(id, parent, value) => SchemeIdentifier(id, parent, value.mkString(""))
+        case AstNumber(id, parent, value) => SchemeNumber(id, parent, value)
+        case AstString(id, parent, value) => SchemeString(id, parent, value.mkString(""))
+      }
+      Edit.Add(hydratedTree, parent, index)
+  }
+}
 
 case class ReplicatedStringOp(editIdentity: (Int, Int), edit: AstEdit[Int])
   extends ReplicatedOperation[Int, (Int, Int)] {
 
-  def serialize(): ByteBuffer = {
-    val repOp: RepOp = edit match {
-      case add: AstEdit.Add[Int] if add.parent.isEmpty =>
-        val e = Array(1, add.index, 0, 0)
-        RepOp(this.editIdentity, e, Some(encodeTree(add.tree)))
-      case add: AstEdit.Add[Int] if add.parent.nonEmpty =>
-        val e = Array(1, add.index, add.parent.get, 1)
-        RepOp(this.editIdentity, e, Some(encodeTree(add.tree)))
-      case delete: AstEdit.Delete[Int] =>
-        val e = Array(2, delete.target)
-        RepOp(this.editIdentity, e, None)
-      case move: AstEdit.Move[Int] =>
-        val e = Array(3, move.child, move.newParent, move.index)
-        RepOp(this.editIdentity, e, None)
-      case value: AstEdit.UpdateValue[Int] =>
-        val e = Array(4, value.target, value.value.asInstanceOf[Long].toInt)
-        RepOp(this.editIdentity, e, None)
-    }
-
-    Pickle.intoBytes(repOp)
-  }
+  def serialize(): ByteBuffer =
+    Pickle.intoBytes(RepIntOp(editIdentity, SerializableAstEdit.astEditToSerializableEdit(edit)))
 
 }
 
@@ -45,31 +74,9 @@ object ReplicatedStringOp {
   def from(v: ReplicatedOperation[Int, (Int, Int)]): ReplicatedStringOp =
     ReplicatedStringOp(v.editIdentity, v.edit)
 
-  private def encodeTree(node: SchemeNode[Int]): Array[Int] = node match {
-    case node: SchemeNumber[Int] if node.parent.nonEmpty => Array(1, node.id, node.value.toInt, 1, node.parent.get)
-    case node: SchemeNumber[Int] if node.parent.isEmpty => Array(1, node.id, node.value.toInt, 0, 0)
-    case node: SchemeExpression[Int] if node.parent.nonEmpty => Array(2, node.id, 1, node.parent.get) ++ node.children
-    case node: SchemeExpression[Int] if node.parent.nonEmpty => Array(2, node.id, 0, 0) ++ node.children
-  }
-
-  protected def decodeTree(node: Array[Int]): SchemeNode[Int] = node match {
-    case Array(1, id, value, 1, parent) => SchemeNumber(id, Some(parent), value)
-    case Array(1, id, value, 0, 0) => SchemeNumber(id, None, value)
-    case Array(2, id, 1, parent, children@_*) => SchemeExpression(id, Some(parent), children)
-    case Array(2, id, 0, 0, children@_*) => SchemeExpression(id, None, children)
-  }
-
-
   def deserialize(buffer: ByteBuffer): ReplicatedStringOp = {
-    val RepOp(ei, e, target) = Unpickle[RepOp].fromBytes(buffer)
-    val edit = e match {
-      case Array(1, index, 0, 0) => Add(decodeTree(target.get), None, index)
-      case Array(1, index, parent, 1) => Add(decodeTree(target.get), Some(parent), index)
-      case Array(2, target) => Delete(target)
-      case Array(3, child, newParent, index) => Move(child, newParent, index)
-      case Array(4, target, value) => UpdateValue(target, value.toLong)
-    }
-    ReplicatedStringOp(ei, edit)
+    val RepIntOp(editIdentity, edit) = Unpickle[RepIntOp].fromBytes(buffer)
+    ReplicatedStringOp(editIdentity, SerializableAstEdit.serializedEditToEdit(edit))
   }
 
 }
