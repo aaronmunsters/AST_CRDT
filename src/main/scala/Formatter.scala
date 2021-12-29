dbc4afb7f4338106c391bd830c3aeab19a8682b8
aaronmunsters <aaronmunstersbxl@gmail.com>
2021-12-29 11:35:21 +0100

    Removed unused Formatter object.

--- src/main/scala/Formatter.scala
+++
@@ -1,9 +0,0 @@
-import AST.Parse.Parser
-
-object Formatter {
-  def format(source: String, indentation: Int = 4): Option[String] = {
-    var uniqueIdentity = 0
-    val getUniqueIdentity = () => { uniqueIdentity += 1; uniqueIdentity }
-    Parser.parse(source, getUniqueIdentity).map(_.toPrettyAstString(indentation))
-  }
-}
